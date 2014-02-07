52eb165b7a3ccbc2b1bbb35be92bde1e94414da4
FaustVX <jodu035@gmail.com>
2014-02-07 18:16:40 +0100

    * [UDPHelper] Modification du Deserializer

--- UDP/UDPHelper.cs
+++ UDP/UDPHelper.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Reflection;
 using System.Runtime.Serialization;
 using System.Text;
 using System.Runtime.Serialization.Formatters.Binary;
@@ -10,49 +11,21 @@ namespace UDP
 {
 	public static class UDPHelper
 	{
-		private class UDPClientBinder : SerializationBinder
+		private class Serializer : SerializationBinder
 		{
-			/// <summary>
-			/// En cas de substitution dans une classe dérivée, contrôle la liaison d'un objet sérialisé avec un type.
-			/// </summary>
-			/// <returns>
-			/// Type de l'objet dont le formateur crée une nouvelle instance.
-			/// </returns>
-			/// <param name="assemblyName">Spécifie le nom <see cref="T:System.Reflection.Assembly"/> de l'objet sérialisé.</param><param name="typeName">Spécifie le nom <see cref="T:System.Type"/> de l'objet sérialisé.</param>
 			public override Type BindToType(string assemblyName, string typeName)
 			{
-				return (typeName == typeof (Client).FullName) ? typeof (Client) : null;
+				var assembly = Assembly.Load(assemblyName);
+				var type = assembly.GetType(typeName);
+				return type;
 			}
 		}
-		private static readonly Dictionary<Type, SerializationBinder> TypeConvertor;
+
+		public static SerializationBinder TypeConvertor { get; private set; }
 
 		static UDPHelper()
 		{
-			TypeConvertor = new Dictionary<Type, SerializationBinder>();
-			AddtypeConvertor<Client>(new UDPClientBinder());
-		}
-
-		/// <summary>
-		/// Add a Type convertor to deserialize object
-		/// </summary>
-		/// <typeparam name="T">Type to convert</typeparam>
-		/// <param name="typeConvertor">Convertor for thee type</param>
-		public static void AddtypeConvertor<T>(SerializationBinder typeConvertor)
-		{
-			AddTypeConvertor(typeof(T), typeConvertor);
-		}
-
-		/// <summary>
-		/// Add a Type convertor to deserialize object
-		/// </summary>
-		/// <param name="type">Type to convert</param>
-		/// <param name="typeConvertor">Convertor for thee type</param>
-		public static void AddTypeConvertor(this Type type, SerializationBinder typeConvertor)
-		{
-			if (TypeConvertor.ContainsKey(type))
-				TypeConvertor[type] = typeConvertor;
-			else
-				TypeConvertor.Add(type, typeConvertor);
+			TypeConvertor = new Serializer();
 		}
 
 		public static byte[] Serialize<T>(this T o)
@@ -71,21 +44,13 @@ namespace UDP
 		public static T Deserialize<T>(this byte[] buffer)
 			where T : class
 		{
-			Type baseType = typeof(T);
-			for (; baseType != null; baseType = baseType.BaseType)
-				if (TypeConvertor.ContainsKey(baseType))
-					break;
-			if (baseType == null)
-				return null;
-
-			SerializationBinder typeConvertor = TypeConvertor[typeof (T)];
-			return Deserialize<T>(buffer, typeConvertor);
+			return Deserialize<T>(buffer, TypeConvertor);
 		}
 
 		public static T Deserialize<T>(this byte[] buffer, SerializationBinder typeConvertor)
 			where T : class
 		{
-			AddtypeConvertor<T>(typeConvertor);
+			//AddtypeConvertor<T>(typeConvertor);
 
 			T returnValue = null;
 			using (MemoryStream memoryStream = new MemoryStream(buffer))
